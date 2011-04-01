b1035a6c0ae07573f89882491a4829d8e327da1b
Eric Melski <ericm@electric-cloud.com>
2011-04-01 00:00:41 -0700

    Load branch spec from another file if present

--- p42gource/p42gource.tcl
+++ p42gource/p42gource.tcl
@@ -1,9 +1,52 @@
 #!tclsh
-
-if { [catch {package require Tcl 8.6}] } {
-    puts stderr "p42gource requires Tcl 8.6 or later"
-    exit 1
-}
+#
+# p42gource
+#
+# Convert a collection of perforce changelist descriptions to a gource-format
+# log.  Usage instructions:
+#
+#       1.  Extract data from perforce.  Find the highest changelist in your
+#           depot, and use something like the following (bash 3.0+):
+#               for n in {1..MAX} ; do
+#                   p4 describe -s $n >> perforce.log
+#               done
+#       2.  Enumerate the branches you are interested in below, one per line
+#           in the "branches" variable.
+#       3.  Run this script as
+#               tclsh p42gource < perforce.log > gource.log
+#
+# Then use the gource custom log format to render the log:
+#
+#       gource --log-format custom gource.log
+#
+# For more information on gource, see http://code.google.com/p/gource/.
+# For more information on perforce, see http://www.perforce.com/
+#
+# Copyright (c) 2011, Eric Melski
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Eric Melski's name may not be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 proc main {} {
     global argv
@@ -19,44 +62,26 @@ proc main {} {
         purge       D
     }
 
-    # List of branches of the depot that we're interested in.
+    # List of branches of the depot that we're interested in.  List one branch
+    # per line, for example:
+    #
+    # set branches {
+    #     //depot/foo/1.0
+    #     //depot/foo/2.0
+    #     //depot/foo/3.0
+    # }
 
     set branches {
-        //depot/ecloud/2.1
-        //depot/ecloud/2.2
-        //depot/ecloud/2.3
-        //depot/ecloud/2.3.0
-        //depot/ecloud/3.0
-        //depot/ecloud/3.0.2
-        //depot/ecloud/3.0.3
-        //depot/ecloud/3.0.4
-        //depot/ecloud/3.5
-        //depot/ecloud/3.6
-        //depot/ecloud/3.7
-        //depot/ecloud/4.0
-        //depot/ecloud/4.1
-        //depot/ecloud/4.2
-        //depot/ecloud/4.3
-        //depot/ecloud/4.4
-        //depot/ecloud/4.5
-        //depot/ecloud/4.6
-        //depot/ecloud/5.0
-        //depot/ecloud/5.1
-        //depot/ecloud/5.2
-        //depot/ecloud/5.3
-        //depot/ecloud/5.4
-        //depot/ecloud/main
-        //depot/ecloud/autodep
-        //depot/ecloud/other
-        //depot/ecloud/other/autodep
-        //depot/ecloud/other/blind
-        //depot/ecloud/other/newcm
-        //depot/ecloud/other/sharedcache
-        //depot/ecloud/other/subbuilds
-        //depot/ecloud/other/versionfs
-        //depot/ecloud/other/orderonly
-        //depot/ecloud/other/registry
-        //depot/ecloud/other/64bit
+
+    }
+
+    if { [file exists branches.tcl] } {
+        source branches.tcl
+    }
+
+    if { [llength $branches] == 0 } {
+        puts stderr "No branches specified."
+        exit 1
     }
 
     # Convert list of branches to a tree of path components, so we can do
@@ -73,6 +98,7 @@ proc main {} {
         set tree($curr) 1
     }
 
+    fconfigure stdin -buffering full -buffersize 16777216
     while { ![eof stdin] } {
         set line [gets stdin]
 
@@ -80,21 +106,22 @@ proc main {} {
             # Skip blank lines.
         }
 
-        if { [string range $line 0 5] == "Change" } {
+        set prefix [string range $line 0 3]
+        if { $prefix == "Chan" } {
             # Found the start of a new changelist.  Extract the user and
             # timestamp.
 
             set change [lindex $line 1]
             set u      [lindex $line 3]
-            set d      [lindex $line 5]
+            set d      [string map [list / -] [lindex $line 5]]
             set t      [lindex $line 6]
 
-            set timestamp [clock scan "$d $t" -format "%Y/%m/%d %H:%M:%S"]
+            set timestamp [clock scan "$d $t"]
             set user      [lindex [split $u @] 0]
             if { $change % 500 == 0 } {
                 puts stderr "Processing $change"
             }
-        } elseif { [string range $line 0 2] == "..." } {
+        } elseif { $prefix == "... " } {
             # Found a filename from the "Affected files ..." section.
 
             set hash [string first # $line]
@@ -117,7 +144,7 @@ proc main {} {
             foreach component $components {
                 append prefix $component/
                 if { ![info exists tree($p,$component)] } {
-                    # Don't recognize this branch, so bail out.
+                    # Don't recognize this branch, so skip to the next file.
 
                     break
                 }
@@ -152,7 +179,7 @@ proc main {} {
             puts "$timestamp|$user|$ops($op)|$filename"
         }
     }
-    puts "Finished through changelist $change"
+    puts stderr "Finished through changelist $change"
 }
 
 main
