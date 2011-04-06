ad4849e707aadf1f72f42bf94e39d9fe262f0f28
Eric Melski <ericm@electric-cloud.com>
2011-04-06 00:08:36 -0700

    Add copyright and license notice

--- p4stats/p4commits.tcl
+++ p4stats/p4commits.tcl
@@ -1,3 +1,51 @@
+#!tclsh
+#
+# p4commits
+#
+# Collect commit stats from a collection of perforce changelist descriptions,
+# outputting the number of commits in the previous 30 days for each 30-day
+# period in the timeframe covered by the changelists.
+#
+#       1.  Extract data from perforce.  Find the highest changelist in your
+#           depot, and use something like the following (bash 3.0+):
+#               for n in {1..MAX} ; do
+#                   p4 describe -s $n >> perforce.log
+#               done
+#       2.  Create a configuration file enumerating the branches you want to
+#           track, one per line.
+#       3.  Run this script as
+#               tclsh p4commits.tcl branches.conf < perforce.log > commits.dat
+#           or, to get stats for a specific user
+#               tclsh p4commits.tcl branches.conf username < perforce.log > commits.dat
+#
+# For more information on perforce, see http://www.perforce.com/
+#
+# Copyright (c) 2011, Eric Melski
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Eric Melski's name may not be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 source util.tcl
 
 proc main {branchFile matchUser} {
